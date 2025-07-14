f673ebb3cc4a42d77ad9f91183fb06c324b0b106
Ridwan Bahtiar <133017656+ridwanbahtiar15@users.noreply.github.com>
2025-07-14 20:11:49 +0700

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -32,7 +32,7 @@ export default function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
       <div className="bg-white p-8 shadow-md w-full max-w-sm rounded-2xl">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
         {error && (
